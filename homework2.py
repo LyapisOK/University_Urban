78289c73db4862d228c2c2ff4f61a4f69452410a
LyapisOK <as@lepunov.ru>
2024-07-14 21:54:29 +0500

    Add files via upload
    Практическое задание по теме "Переменные."

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Number_of_completed = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = Number_of_completed / Number_of_hours_spent
+print('Курс:', Course_name + ',', 'всего задач:' + str(Number_of_completed) + ',', 'затрачено часов:' + str(Number_of_hours_spent) + ',', 'среднее время выполнения', Time_for_one_task, 'часов')

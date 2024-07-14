9facc582ce57c70e049f979fb61f272552b23ba8
LyapisOK <as@lepunov.ru>
2024-07-14 21:26:21 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#1st program
+
+print(9 ** 0.5 * 5)
+
+#2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+
+a = 1234 % 1000 // 10
+b = 5678 % 1000 // 10
+print(a + b)
+
+#4th program
+
+a = 13.42
+b = 42.13
+print(a * 100 // 100 == b * 100 % 100 or a * 100 % 100 == b * 100 // 100)

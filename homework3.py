8766aabeaecaf5d5963ae06a568cdf2f6b7d757b
LyapisOK <as@lepunov.ru>
2024-07-15 16:43:33 +0500

    Add files via upload
    Практическая работа по уроку "Динамическая типизация"

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Sasha'
+print('Name:', name, type(name))
+age = 41
+print('Age:', age, type(age))
+age = age + 5
+print('New age:', age, type(age))
+is_student = True
+print('Is Student:', is_student, type(is_student))

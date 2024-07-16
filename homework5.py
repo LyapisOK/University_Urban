0ea2d1d8a4d4c82c922cdfbcc8c6d5271f54133b
LyapisOK <as@lepunov.ru>
2024-07-16 06:38:30 +0500

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -4,8 +4,8 @@ print('Immutable var:', immutable_var)
 #immutable_var[0] = 3 в кортеже нельзя изменить неизменяемые типы данных (числа, строки)
 #print(immutable_var)
 
-mutable_list = ([1, 2, 'Student', True], [9, 8, 'Teacher', False])
+mutable_list = [[1, 2, 'Student', True], 9, 8, 'Teacher', False]
 print('Mutable list:', mutable_list)
 mutable_list[0][1] = 2024
-mutable_list[1][3] = 'Ложь'
+mutable_list[4] = 'Ложь'
 print('New Mutable list:', mutable_list)

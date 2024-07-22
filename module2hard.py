66deb1a0e8a9404198948d2c49eab7a650876b48
LyapisOK <as@lepunov.ru>
2024-07-22 20:48:36 +0500

    module2hard.py
    Исправлено, добавлена проверка на зеркальность пар

--- module2hard.py
+++ module2hard.py
@@ -1,10 +1,26 @@
-n = int(input())
-result = []
-
-for i in range(1, n//2+1):
-    for j in range(1, n+1):
-        if n % (i + j) == 0:
+def pairs_of_numbers(n):
+    res = []
+    res_ = []
+    result = []
+    for i in range(1, n+1):
+        for j in range(1, n+1):
             if i != j:
-                result += [i, j]
-            
-print(*result, sep = '')
+                if n % (i + j) == 0:
+                    res_.append(j)
+                    res_.append(i)
+                    if res_ not in result:
+                        res.append(i)
+                        res.append(j)
+                        result.append(res)
+            res = []
+            res_ = []
+
+    return result
+
+answer = pairs_of_numbers(int(input('Введите число от 3 до 20: ')))
+answer_ = ''
+for h in answer:
+    for k in h:
+        answer_ += str(k)
+
+print(answer_)

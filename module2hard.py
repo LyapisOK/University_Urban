b623f48d606956fcfa8914572381df4b253dde1a
LyapisOK <as@lepunov.ru>
2024-07-23 21:57:41 +0500

    module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,20 +1,15 @@
 def pairs_of_numbers(n):
     res = []
-    res_ = []
     result = []
     answer = ''
-    for i in range(1, n+1):
-        for j in range(1, n+1):
+    for i in range(1, n):
+        for j in range(i+1, n+1):
             if i != j:
                 if n % (i + j) == 0:
-                    res_.append(j)
-                    res_.append(i)
-                    if res_ not in result:
-                        res.append(i)
-                        res.append(j)
-                        result.append(res)
+                    res.append(i)
+                    res.append(j)
+                    result.append(res)
             res = []
-            res_ = []
     for h in result:
         for k in h:
             answer += str(k)

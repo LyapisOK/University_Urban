6e87c982ea66bd7e71a1b480fc8460f6cc5e2538
Alexander <as@lepunov.ru>
2024-08-12 16:49:45 +0500

    Update module_5_3.py
    НАйдена ошибка

--- module_5_3.py
+++ module_5_3.py
@@ -13,7 +13,7 @@ class House:
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        return self.number_of_floors == other
     def __lt__(self, other):
         return self.number_of_floors < other.number_of_floors
     def __le__(self, other):
@@ -25,11 +25,11 @@ class House:
     def __ne__(self, other):
         return self.number_of_floors != other.number_of_floors
     def __add__(self, other):
-        return self.number_of_floors + other.number_of_floors
+        return self.number_of_floors + other
     def __radd__(self, other):
-        return other.number_of_floors + self.number_of_floors
-    def __radd__(self):
-        return self.number_of_floors+10
+        return other + self.number_of_floors
+    def __iadd__(self):
+        return self.number_of_floors + 10
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)

415d86c836125fc25041e97a764f1eeebd98ba3a
Alexander <as@lepunov.ru>
2024-09-19 20:36:53 +0500

    Update module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -9,7 +9,7 @@ class WordsFinder:
         not_simbol = [',', '.', '=', '!', '?', ';', ':', ' - ']
 
         for file_name in self.file_names:
-            print(file_name)
+            #print(file_name)
             with open(file_name, 'r', encoding='utf-8') as file:
                 file_str = (file.read().lower())
                 for i in not_simbol:
@@ -20,10 +20,24 @@ class WordsFinder:
                 #print(new_str)
         return all_words
     def find(self, word):
+        word_find = {}
         self.word = word
-        for name, words in get_all_words().items():
+        for name, words in self.get_all_words().items():
+            if word.lower() in words:
+                word_find[name] = words.index(word.lower()) + 1
+        return  word_find
     def count(self, word):
-
+        word_count = {}
+        count = 0
+        for name, words in self.get_all_words().items():
+            for i in words:
+                if i == word.lower():
+                    count += 1
+            word_count[name] = count
+        return word_count
 
 finder2 = WordsFinder('test.txt', 'Sample.txt')
-print(finder2.get_all_words())
+
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего

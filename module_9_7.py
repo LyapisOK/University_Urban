27db35b40afe2dda77aa15e8d6abbe1abaabcc38
Alexander <as@lepunov.ru>
2024-10-08 11:43:51 +0500

    Add files via upload
    Домашнее задание по теме "Декораторы"

---
+++ module_9_7.py
@@ -0,0 +1,21 @@
+def is_prime(func):
+    def wrapper(*args):
+        number = func(*args)
+
+        for i in range(2, int(number ** 0.5) + 1):
+            if number % i == 0:
+                print('Составное')
+                break
+            else:
+                print('Простое')
+                break
+        return number
+
+    return wrapper
+
+@is_prime
+def sum_three(*args):
+    return sum(args)
+
+result = sum_three(2, 3, 6)
+print(result)
